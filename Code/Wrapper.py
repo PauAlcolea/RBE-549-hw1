3230f151b32a5a1f80d1d56cf56c8914cc712966
Pau Alcolea Vila <paualcoleavila@autoreg-6815132.dyn.wpi.edu>
2026-02-09 21:27:22 -0500

    get corners out of calibration images

--- Code/Wrapper.py
+++ Code/Wrapper.py
@@ -22,6 +22,31 @@ def initialize_calib(images_dir=str):
     
     return calib_images
 
+"""
+get_corners takes a list of images and gets the corners
+"""
+def get_corners(images: list[np.ndarray], grid_shape: tuple[int, int]):
+    corners_list = []
+
+    for im in images:
+        # Shape (6,9) is inner grid and is in Height (y) and width (x)
+        ret, corners = cv2.findChessboardCorners(im, grid_shape, None)
+        
+        #if it returned the proper corners
+        if ret == True:
+            corners2D = corners.reshape(-1,2)
+            corners_list.append(corners2D)
+            
+            # # Visualize Corners
+            # with_corners = cv2.drawChessboardCorners(im, grid_shape, corners, ret)
+            # cv2.imshow("cornered", with_corners)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+        else:
+            print("Proper corners with specified grid shape have not been identified")
+            continue
+
+    return corners_list
 
 def main():
     # get path to current directory
@@ -37,22 +62,24 @@ def main():
     images_dir = args.dir
 
 
-    # calib_corners are the four corners of the unwarped actual checkerboard pattern, points go counter clockwise from buttom left 
-    # sq_sz = 21.5
-    # calib_x = 8
-    # calib_y = 5
+    # reference2D
+    sq_sz = 21.5
+    h = 6
+    w = 9
+    grid_shape = (h, w)
 
+    reference = np.zeros((h, w, 2), dtype=np.float32)
+    for i in range(h):
+        for j in range(w):
+            reference[i][j][0] = sq_sz * i
+            reference[i][j][1] = sq_sz * j
 
+    reference2D = reference.reshape(-1, 2)
 
-    # Initialize all of the images that will be used for calibration, they are in dir = "/../Calib_Imgs"   
+    # Initialize all of the images that will be used for calibration, they are in dir = "/../Calib_Imgs" and get the points in the grid
     imgs = initialize_calib(images_dir)
-    for im in imgs:
-        ret, corners = cv2.findChessboardCorners(im, (9,6), None)
-        if ret == True:
-            with_corners = cv2.drawChessboardCorners(im, (9,6), corners, ret)
-            cv2.imshow("cornered", with_corners)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+    corners = get_corners(imgs, grid_shape)
+
 
     # Solve for approximate K (camera calibration matrix)
     # Section 3.1 of paper
